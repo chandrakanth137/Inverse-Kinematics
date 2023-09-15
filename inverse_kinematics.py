00432c9d41a84f410eead0829709a89645f1f5d7
Chandrakanth <apcmchandu@gmail.com>
2023-09-15 11:11:50 +0530

    verfied ik, modified print and added comments

--- inverse_kinematics.py
+++ inverse_kinematics.py
@@ -1,14 +1,36 @@
-from math import acos, sin, cos, pi, degrees, atan2
+'''
+The following program calculates the inverse kinematics of a 2 arm manipulator robot
+It also verifies the joint angles by calculating the forward kinematics based on the calculated joint angles from inverse kinematics
+
+Given the flexibilty, a single point can be achieved using 2 different positions for the link arms, so the inverse kinematics for both 
+the configuration is calculated. [DOF_2_config1 & DOF_2_config2]
+'''
+
+from math import acos, sin, cos, degrees, atan2
+
+'''
+The following class represents one type of arm configuration to calculate inverse kinematics.
+X - X coordinate of the end effector
+Y - Y coordinate of the end effector
+L1 - Length of the first link / arm
+L2 - Length of the second link / arm
+theta_1 - Joint angle of the first link / arm
+theta_2 - Joint angle of the second link / arm 
+
+The function "second_arm_joint_angle" calculates theta_2
+The function "first_arm_joint_angle" calculates theta_1
+The function "forward kinematics" calculates the forward kinematics based on the calculated joint angles from inverse kinematics
+The function "calc_joint_angles" is the driver function for the class which calls the other functions and prints the value 
+'''
 
 class DOF_2_config1:
     def __init__(self, L1, L2, X, Y):
-        self.X = X
+        self.X = X 
         self.Y = Y
         self.L1 = L1
         self.L2 = L2
         self.theta_1 = None
         self.theta_2 = None
-
     def second_arm_joint_angle(self):
         numerator = (self.X ** 2) + (self.Y ** 2) - (self.L1 ** 2) - (self.L2 ** 2)
         denominator = 2 * self.L1 * self.L2
@@ -32,16 +54,18 @@ class DOF_2_config1:
     def calc_joint_angles(self):
         self.second_arm_joint_angle()
         self.first_arm_joint_angle()
-        print(self.theta_1 / self.theta_2)
-        print("--------------Calculating Inverse Kinematics for Type I Configuration-----------------------")
+        print("============================= Configuration 1 =============================================\n")
+        print("-------------------------- Calculating Inverse Kinematics --------------------------------")
         print("The joint angle of the first arm: ", degrees(self.theta_1))
         print("The joint angle of the second arm: ", degrees(self.theta_2))
-        print("--------------------------------------------------------------------------------------------")
+        print("--------------------------------------------------------------------------------------------\n")
+        
         X,Y = self.forward_kinematics()
-        print("--------------Calculating Forward Kinematics for Type I Configuration-----------------------")
+        print("-------------------------- Calculating Forward Kinematics ---------------------------------")
         print("Coordinate X: ", X)
         print("Coordinate Y: ", Y)
         print("---------------------------------------------------------------------------------------------")
+        print("===========================================================================================\n")
 
 class DOF_2_config2:
     def __init__(self, L1, L2, X, Y):
@@ -76,23 +100,24 @@ class DOF_2_config2:
         self.second_arm_joint_angle()
         self.first_arm_joint_angle()
         
-        print(self.theta_1 / self.theta_2)
-        print("--------------Calculating Inverse Kinematics for Type II Configuration-----------------------")
+        print("============================= Configuration 2 =============================================\n")
+        print("-------------------------- Calculating Inverse Kinematics --------------------------------")
         print("The joint angle of the first arm: ", degrees(self.theta_1))
         print("The joint angle of the second arm: ", degrees(self.theta_2))
-        print("---------------------------------------------------------------------------------------------")
+        print("--------------------------------------------------------------------------------------------\n")
+        
         X,Y = self.forward_kinematics()
-        print("--------------Calculating Forward Kinematics for Type II Configuration-----------------------")
+        print("-------------------------- Calculating Forward Kinematics ---------------------------------")
         print("Coordinate X: ", X)
         print("Coordinate Y: ", Y)
         print("---------------------------------------------------------------------------------------------")
-        
+        print("===========================================================================================\n")
 
 if __name__ == '__main__':
-    X = int(input("Enter the X Coordinate: "))
-    Y = int(input("Enter the Y Coordinate: "))
-    L1 = int(input("Enter the length of first arm: "))
-    L2 = int(input("Enter the length of second arm: "))
+    X = float(input("Enter the X Coordinate: "))
+    Y = float(input("Enter the Y Coordinate: "))
+    L1 = float(input("Enter the length of first arm: "))
+    L2 = float(input("Enter the length of second arm: "))
 
     config1 = DOF_2_config1(L1,L2,X,Y)
     config2 = DOF_2_config2(L1,L2,X,Y)
